fbd063a3908e2bf105a595b8b3b666dc88b4de90
Schnappo90 <alexwilliamknapp@gmail.com>
2026-03-30 21:48:00 +0100

    Add share wrapper functionality to close share menu on outside click

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (() => {
   const socialMenu = document.getElementById("share-menu");
   const shareButton = document.querySelector("button");
+  const shareWrapper = document.querySelector(".share-wrapper");
 
   if (!socialMenu || !shareButton) {
     return console.error("Required elements not found");
@@ -11,5 +12,16 @@
     socialMenu.classList.toggle("open");
 
     shareButton.ariaExpanded = String(shareButton.classList.contains("open"));
+  });
+
+  document.addEventListener("click", (e) => {
+    if (
+      socialMenu.classList.contains("open") &&
+      !shareWrapper.contains(e.target)
+    ) {
+      shareButton.classList.remove("open");
+      socialMenu.classList.remove("open");
+      shareButton.ariaExpanded = "false";
+    }
   });
 })();
